f452e64230724335a5e03b605b16f77adf1e59c8
Vasco Neves <vasconeves@Vascos-MacBook-Pro.local>
2025-07-24 21:27:12 +0100

    setup frontend and clean up backend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
